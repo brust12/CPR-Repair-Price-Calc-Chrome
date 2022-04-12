2016affed8f1ece85d7f39965e739b3ba7558782
Riley B <33437467+brust12@users.noreply.github.com>
2022-04-11 22:32:27 -0500

    added info for labor

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,7 +1,6 @@
 
 
-function calcRepair(partcost,isTablet){
-	var labor = isTablet ? 75 : 55;
+function calcRepair(partcost,labor){
 	var mult;
 	if(partcost <= 9.99){
 		mult = 5;
@@ -16,63 +15,51 @@ function calcRepair(partcost,isTablet){
 	}else if (partcost >200){
 		mult =1.25;
 	}
-	var partc = (partcost*mult)+labor;
-	var rounded = Math.ceil(partc/10)*10;
+	var price = (partcost*mult)+labor;
+	var rounded = Math.ceil(price/10)*10;
 	return Math.round(rounded)-.01;
-
 }
 
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-		//Trying to check if more items are fetched
-		document.addEventListener("load",function(){
-			console.log("List changed!");
-		},false)
-		
-
-		var URL = document.URL;
-		// if(URL.includes("mobiledefenders")){
-		// 	console.log("TRUE")
-		// }
-
-		
-		var isTablet;
+		var labor = 55;
+		var device_type = "PHONE";
 		if(document.URL.includes("ipad")){
-			isTablet = true;
-		}else{
-			isTablet = false;
+			device_type = "TABLET";
+			labor = 75;
 		}
-		console.log(isTablet);
+		if(document.URL.includes("game-console")){
+			device_type = "CONSOLE";
+			labor =100;
+		}
+		console.log(device_type);
 
 		var elements = document.getElementsByClassName("price"),i,len;
 		for(i=0,len = elements.length; i<len; i++){
-			if(elements[i].parentElement.className =="old-price" || elements[i].parentElement.parentElement.id == "cartblock"){
+			if(elements[i].parentElement.className =="old-price" || elements[i].parentElement.parentElement.id == "cartblock"){ //remove prices for sale and cart on Sentrix
 				continue;
 			}
-			var price = elements[i].textContent;
-			price = price.replace('$','')
+			var cost = elements[i].textContent;
+			cost = cost.replace('$','')
 
 
-			var num = calcRepair(Number(price),isTablet); 
+			var num = calcRepair(Number(cost),labor); 
 
 			var span = document.createElement('span');
 			var part = document.createElement('div');
-
 			part.style.fontSize ="smaller";
 			span.style.color = "red"; 
 
 			span.appendChild(document.createTextNode(" Repair Price: $"+num));
-			var part_price = num - 55;;
-			part.appendChild(document.createTextNode("Part Price: $"+Number(part_price.toPrecision(2)-.01)));
+			var part_price = num - labor;;
+			part.appendChild(document.createTextNode("Part Price: $"+Number(part_price.toPrecision(2)-.01)+" Labor: $"+labor));
 
 			var parent = elements[i].parentElement.parentElement.parentElement;
 
 			parent.insertBefore(span,parent.lastChild);
 			parent.insertBefore(part,parent.lastChild);
-
-
 		}
 	}
 	}, 10);
