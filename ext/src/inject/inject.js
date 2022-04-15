8b583bd54756f39850dfeee187df3e752ae217c4
Riley B <33437467+brust12@users.noreply.github.com>
2022-04-15 15:31:59 -0500

    cleaned up code and formatted

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -8,13 +8,13 @@ function calcRepair(partcost,labor){
 		mult = 5;
 	}else if(partcost >10 &  partcost<=24.99){
 		mult = 2.5;
-	}else if(partcost>25  &  partcost<=49.99) {
+	}else if(partcost >25  &  partcost<=49.99) {
 		mult =2.25;
-	}else if(partcost>50  &  partcost<=99.99){
+	}else if(partcost >50  &  partcost<=99.99){
 		mult = 2.00;
-	}else if(partcost>100 &  partcost<=199.99){
+	}else if(partcost >100 &  partcost<=199.99){
 		mult = 1.5;
-	}else if (partcost >200){
+	}else if(partcost >200){
 		mult =1.25;
 	}
 	var price = (partcost * mult) + labor;
@@ -26,29 +26,29 @@ function insert(){
     //Check to see if were on a parts page or another page.
     let url = document.URL;
     const blacklist =["tools","brands/","refurbishing","accessories","checkout"];
-    if(blacklist.some((word)=>url.includes(word))) return;
+    if(blacklist.some((word) => url.includes(word))) return;
 
     //Set Labor 
     const tablets = ["ipad","surface","galaxy-tab"];
     const consoles = ["game-console","sony","xbox","nintendo","macbook-parts"];
     var labor = 55;
-    if(tablets.some((word)=>url.includes(word)))    labor = 75;
-    if(consoles.some((word)=>url.includes(word)))   labor =100;
+    if(tablets.some((word) => url.includes(word)))    labor = 75;
+    if(consoles.some((word)=> url.includes(word)))    labor = 100;
 
     //ADDING PRICES
     var elements = document.getElementsByClassName("price"),i,len;
     for(const part_item of elements){ //Looping through each price element on the page.
         let parentclass = part_item.parentElement.className;
-        if(parentclass=="old-price") continue; //skip if the price is a sale item.
+        if(parentclass == "old-price") continue; //skip if the price is a sale item.
 
         //Checking if the parent class is one that would contain parts or skipping if its another price. EX: the cart price.
         let parentClass4 = part_item.parentElement.parentElement.parentElement.parentElement;
         let parentClass3 = part_item.parentElement.parentElement.parentElement;
-        if(!(parentClass4.className == "catalog-view-details-grid-hover"    //MD items page
-        || parentClass3.className == "cat_prod_cell"                        //MS items page
-        || parentClass4.className == "detailpage"                           //MS part page
-        || parentClass4.className == "product-info")) continue;             //MD part page
-
+        if(!(parentClass4.className == "catalog-view-details-grid-hover"      //MD items page
+        || parentClass3.className   == "cat_prod_cell"                        //MS items page
+        || parentClass4.className   == "detailpage"                           //MS part page
+        || parentClass4.className   == "product-info")) continue;             //MD part page
+        
         var cost = part_item.textContent;
         cost = cost.replace('$','');
         var repair_price = calcRepair(Number(cost),labor); 
@@ -63,7 +63,7 @@ function insert(){
             repair_div.style.paddingTop = "5px";
             repair_div.style.paddingBottom = "5px";
         }
-        //ADDING HTML ELEMENTS TO THE PAGE
+        //Inserting the price elements
         var breakr = document.createElement("br");// for moving the price to a new line on Sentrix part page.
         repair_div.appendChild(document.createTextNode(" Repair Price: $"+repair_price),breakr);
         var part_price = repair_price - labor;
